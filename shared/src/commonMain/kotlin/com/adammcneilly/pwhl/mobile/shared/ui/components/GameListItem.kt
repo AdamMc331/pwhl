12dea334f4656482e490ebd6a0193522fb4e4e18
Adam McNeilly <amcneilly331@gmail.com>
2025-11-11 16:20:28 -0500

    Param name update.

--- shared/src/commonMain/kotlin/com/adammcneilly/pwhl/mobile/shared/ui/components/GameListItem.kt
+++ shared/src/commonMain/kotlin/com/adammcneilly/pwhl/mobile/shared/ui/components/GameListItem.kt
@@ -99,7 +99,7 @@ private fun TeamRow(
                 modifier = Modifier
                     .size(PWHLTheme.dimensions.imageSizeUltraCompact)
                     .sharedElement(
-                        state = rememberSharedContentState(key = "${teamGameResult.team.name}_${gameId}_image"),
+                        sharedContentState = rememberSharedContentState(key = "${teamGameResult.team.name}_${gameId}_image"),
                         animatedVisibilityScope = LocalNavAnimatedVisibilityScope.current,
                     ),
             )
