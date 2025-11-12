cd9419d3251f321b1b79c19de05ac038c4d7d349
Adam McNeilly <amcneilly331@gmail.com>
2025-11-12 17:39:40 -0500

    Updating import.

--- shared/src/commonMain/kotlin/com/adammcneilly/pwhl/mobile/shared/time/SystemTimeProvider.kt
+++ shared/src/commonMain/kotlin/com/adammcneilly/pwhl/mobile/shared/time/SystemTimeProvider.kt
@@ -1,7 +1,7 @@
 package com.adammcneilly.pwhl.mobile.shared.time
 
 import kotlinx.datetime.Clock
-import kotlinx.datetime.Instant
+import kotlin.time.Instant
 
 /**
  * This implementation of [TimeProvider] will always return the current system time.
