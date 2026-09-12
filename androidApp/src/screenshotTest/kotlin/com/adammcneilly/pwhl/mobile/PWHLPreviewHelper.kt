3cf48c89df9af0fc0b5201932ce0cbe7fbd142d6
Adam McNeilly <amcneilly331@gmail.com>
2026-09-11 22:15:59 -0400

    Adding feed content previews.

--- androidApp/src/screenshotTest/kotlin/com/adammcneilly/pwhl/mobile/PWHLPreviewHelper.kt
+++ androidApp/src/screenshotTest/kotlin/com/adammcneilly/pwhl/mobile/PWHLPreviewHelper.kt
@@ -2,10 +2,14 @@ package com.adammcneilly.pwhl.mobile
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.SharedTransitionLayout
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
 import com.adammcneilly.pwhl.mobile.shared.LocalNavAnimatedVisibilityScope
 import com.adammcneilly.pwhl.mobile.shared.LocalSharedTransitionScope
+import com.adammcneilly.pwhl.mobile.shared.ui.components.PWHLScreenScaffold
 import com.adammcneilly.pwhl.mobile.shared.ui.theme.Dimensions
 import com.adammcneilly.pwhl.mobile.shared.ui.theme.LocalDimensions
 import com.adammcneilly.pwhl.mobile.shared.ui.theme.PWHLTheme
@@ -31,6 +35,33 @@ fun PWHLPreviewHelper(
                 PWHLTheme {
                     content()
                 }
+            }
+        }
+    }
+}
+
+/**
+ * A helper composable that provides the [LocalSharedTransitionScope] and
+ * [LocalNavAnimatedVisibilityScope] required by components that use
+ * shared element transitions.
+ *
+ * Unlike [PWHLPreviewHelper], this includes a [PWHLScreenScaffold]
+ * useful for testing larger screen content previews that we want to see
+ * the surface background.
+ */
+@Composable
+fun PWHLScreenPreviewHelper(
+    content: @Composable () -> Unit,
+) {
+    PWHLPreviewHelper {
+        PWHLScreenScaffold(
+            title = "Preview Screen",
+        ) { contentPadding ->
+            Box(
+                modifier = Modifier
+                    .padding(contentPadding),
+            ) {
+                content()
             }
         }
     }
--- androidApp/src/screenshotTest/kotlin/com/adammcneilly/pwhl/mobile/displaymodels/TestGameSummaryDisplayModel.kt
+++ androidApp/src/screenshotTest/kotlin/com/adammcneilly/pwhl/mobile/displaymodels/TestGameSummaryDisplayModel.kt
@@ -1,11 +1,28 @@
 package com.adammcneilly.pwhl.mobile.displaymodels
 
 import com.adammcneilly.pwhl.mobile.shared.displaymodels.GameSummaryDisplayModel
+import com.adammcneilly.pwhl.mobile.shared.displaymodels.TeamGameSummaryResultDisplayModel
 
-val testGameSummaryDisplayModel = GameSummaryDisplayModel(
+val testCompletedGameSummaryDisplayModel = GameSummaryDisplayModel(
     id = "123",
     homeTeam = testGameWinnerDisplayModel,
     awayTeam = testGameLoserDisplayModel,
     status = "Final",
+    dateString = "2026-01-01",
+)
+
+val testUpcomingGameSummaryDisplayModel = GameSummaryDisplayModel(
+    id = "123",
+    homeTeam = TeamGameSummaryResultDisplayModel(
+        team = testFleetDisplayModel,
+        goals = 0,
+        isWinner = false,
+    ),
+    awayTeam = TeamGameSummaryResultDisplayModel(
+        team = testSirensDisplayModel,
+        goals = 0,
+        isWinner = false,
+    ),
+    status = "TODO",
     dateString = "2026-01-01",
 )
