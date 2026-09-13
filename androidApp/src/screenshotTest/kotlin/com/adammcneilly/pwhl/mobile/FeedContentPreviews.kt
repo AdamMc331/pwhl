5c431780ebcf924a30af8c6dd1451440f0a74d57
Adam McNeilly <amcneilly331@gmail.com>
2026-09-12 22:47:06 -0400

    Updating previews again.

--- androidApp/src/screenshotTest/kotlin/com/adammcneilly/pwhl/mobile/FeedContentPreviews.kt
+++ androidApp/src/screenshotTest/kotlin/com/adammcneilly/pwhl/mobile/FeedContentPreviews.kt
@@ -15,44 +15,50 @@ import com.android.tools.screenshot.PreviewTest
 @PreviewLightDark
 @PreviewTest
 private fun FeedContentLoadedPreview() {
-    FeedContent(
-        state = FeedState(
-            loadingRecentGames = false,
-            loadingUpcomingGames = false,
-            recentGames = mapOf(
-                "Recent" to listOf(
-                    testCompletedGameSummaryDisplayModel,
-                    testCompletedGameSummaryDisplayModel,
-                    testCompletedGameSummaryDisplayModel,
+    PWHLPreviewHelper {
+        FeedContent(
+            state = FeedState(
+                loadingRecentGames = false,
+                loadingUpcomingGames = false,
+                recentGames = mapOf(
+                    "Recent" to listOf(
+                        testCompletedGameSummaryDisplayModel,
+                        testCompletedGameSummaryDisplayModel,
+                        testCompletedGameSummaryDisplayModel,
+                    ),
+                ),
+                upcomingGames = mapOf(
+                    "Upcoming" to listOf(
+                        testUpcomingGameSummaryDisplayModel,
+                        testUpcomingGameSummaryDisplayModel,
+                        testUpcomingGameSummaryDisplayModel,
+                    ),
                 ),
             ),
-            upcomingGames = mapOf(
-                "Upcoming" to listOf(
-                    testUpcomingGameSummaryDisplayModel,
-                    testUpcomingGameSummaryDisplayModel,
-                    testUpcomingGameSummaryDisplayModel,
-                ),
-            ),
-        ),
-        onGameClicked = {},
-        contentPadding = PaddingValues(),
-        modifier = Modifier
-            .fillMaxSize(),
-    )
+            onGameClicked = {},
+            contentPadding = PaddingValues(),
+            modifier = Modifier
+                .fillMaxSize(),
+        )
+    }
 }
 
 @Composable
 @PreviewLightDark
 @PreviewTest
 private fun FeedContentLoadingPreview() {
-    FeedContent(
-        state = FeedState(
-            loadingRecentGames = true,
-            loadingUpcomingGames = true,
-            recentGames = emptyMap(),
-            upcomingGames = emptyMap(),
-        ),
-        onGameClicked = {},
-        contentPadding = PaddingValues(),
-    )
+    PWHLPreviewHelper {
+        FeedContent(
+            state = FeedState(
+                loadingRecentGames = true,
+                loadingUpcomingGames = true,
+                recentGames = emptyMap(),
+                upcomingGames = emptyMap(),
+            ),
+            onGameClicked = {},
+            contentPadding = PaddingValues(),
+            modifier = Modifier
+                .fillMaxSize(),
+        )
+    }
 }
