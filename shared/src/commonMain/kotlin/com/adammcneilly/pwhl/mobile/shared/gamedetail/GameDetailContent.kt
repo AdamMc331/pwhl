f8c00e358796c7e646ead5dcef218f7160b16086
Adam McNeilly <amcneilly331@gmail.com>
2026-09-13 12:13:43 -0400

    Game detail scaffold (#293)

--- shared/src/commonMain/kotlin/com/adammcneilly/pwhl/mobile/shared/gamedetail/GameDetailContent.kt
+++ shared/src/commonMain/kotlin/com/adammcneilly/pwhl/mobile/shared/gamedetail/GameDetailContent.kt
@@ -10,12 +10,32 @@ import androidx.compose.ui.Modifier
 import com.adammcneilly.pwhl.mobile.shared.displaymodels.GameDetailDisplayModel
 import com.adammcneilly.pwhl.mobile.shared.displaymodels.PlayByPlayEventDisplayModel
 import com.adammcneilly.pwhl.mobile.shared.gamedetail.xr.ImmersiveGameDetailContent
+import com.adammcneilly.pwhl.mobile.shared.scaffold.PersistentScaffold
+import com.adammcneilly.pwhl.mobile.shared.scaffold.rememberScaffoldState
 import com.adammcneilly.pwhl.mobile.shared.ui.components.LoadingScreen
 import com.adammcneilly.pwhl.mobile.shared.ui.theme.PWHLTheme
 import com.adammcneilly.pwhl.mobile.shared.xr.LocalXRSession
 
 @Composable
 fun GameDetailContent(
+    state: GameDetailState,
+    eventHandler: (GameDetailUiEvent) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    rememberScaffoldState().PersistentScaffold(
+        modifier = modifier,
+    ) { scaffoldPadding ->
+        Content(
+            state = state,
+            eventHandler = eventHandler,
+            modifier = Modifier
+                .padding(scaffoldPadding),
+        )
+    }
+}
+
+@Composable
+private fun Content(
     state: GameDetailState,
     eventHandler: (GameDetailUiEvent) -> Unit,
     modifier: Modifier = Modifier,
